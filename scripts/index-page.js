bcfe8047e8685d068ac67c18489e1189d4a9e6b1
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-10-15 00:52:58 -0400

    js-main-page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,1 +1,23 @@
 console.log(":bye");
+
+let userComment=[
+  {
+    name: "Mon Sept 06 2021",
+    timeStamp: "Ronald Lane",
+    commentText: "USA",
+  },
+  {
+    name: "Mon Sept 06 2021",
+    timeStamp: "Ronald Lane",
+    commentText: "Canada",
+  },
+  {
+    name: "Mon Sept 06 2021",
+    timeStamp: "Ronald Lane",
+    commentText: "India",
+  }
+]
+
+let displayComment = function(comment[i].commentText){
+
+}
