cb2f4384d514ad56bf07fb2c670e79b524ba70b9
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-10-16 18:36:31 -0400

    mob-view-form

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,64 +5,91 @@ let userDetail = [
   {
     name: "Connor Walton",
     date: "02/17/2021",
-     commentText: "This is Attr. This is inexplicable magic expressed in the purest WebAssembly, everything that makes up this majestic work deserve reverence. Let us appreciate this for what it is and what it contains.",
-    image:("./assets/images/Mohan-muruge.jpg"),
-    },
+    commentText:
+      "This is Attr. This is inexplicable magic expressed in the purest WebAssembly, everything that makes up this majestic work deserve reverence. Let us appreciate this for what it is and what it contains.",
+    image: "./assets/images/Mohan-muruge.jpg",
+  },
   {
     name: "Emilie Beach",
     date: "01/09/2021",
-    commentText:"I feel blessed to have sen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    image:("./assets/images/Mohan-muruge.jpg"),
+    commentText:
+      "I feel blessed to have sen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    image: "./assets/images/Mohan-muruge.jpg",
   },
   {
     name: "Miles Acosta",
     date: "12/20/2020",
-    commentText:"I can't stop listening.Every time I hear one of their songs- the vocals- it gives me goosebimps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough",
-    image:("./assets/images/Mohan-muruge.jpg"),
+    commentText:
+      "I can't stop listening.Every time I hear one of their songs- the vocals- it gives me goosebimps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough",
+    image: "./assets/images/Mohan-muruge.jpg",
   },
 ];
 
-//DOM Elements
-const conversationUserRight = document.querySelector(".conversation__user-right");
+const conversationUser = document.querySelector(".conversation__user");
 const conversationForm = document.querySelector(".conversation__form");
 
 function conversationDetail() {
-  conversationUserRight.innerHTML = "";
+  conversationUser.innerHTML = "";
   for (let i = 0; i < userDetail.length; i++) {
-    //create <li>
-    const userCommentDisplay = document.createElement("li");
-    userCommentDisplay.innerText = userDetail[i].commentText;
-    userCommentDisplay.classList.add("conversation__user-right-comment-display");
+    const conversationUserRight = document.createElement("div");
+    conversationUserRight.classList.add("conversation__user-right");
 
 
-    const userNameDisplay = document.createElement("li");
-    userNameDisplay.innerText = userDetail[i].name;
-    userNameDisplay.classList.add("conversation__user-right-name-display");
-
-
-    const userDateDisplay = document.createElement("li");
-    userDateDisplay.innerText =userDetail[i].date;
-    userDateDisplay.classList.add("conversation__user-right-date-display");
-
-    const userImageDisplay = document.createElement("img");
-    userImageDisplay.setAttribute("src",userDetail[i].image)
+    const userImageDisplay = document.createElement("div");
     userImageDisplay.classList.add("conversation__user-right-image-display");
-
-    //append
+    const userImageInside = document.createElement("img");
+    userImageInside.setAttribute("src", userDetail[i].image);
+    userImageInside.classList.add("conversation__user-right-image-display--inside");
+    userImageDisplay.appendChild(userImageInside);
     conversationUserRight.appendChild(userImageDisplay);
-    conversationUserRight.appendChild(userNameDisplay);
-    conversationUserRight.appendChild(userCommentDisplay);
-    conversationUserRight.appendChild(userDateDisplay);
 
+
+    const userCommentNameDate = document.createElement("div");
+    userCommentNameDate.classList.add("conversation__user-right-comment-name-date" );
+
+    const userNameDate = document.createElement("div");
+    userNameDate.classList.add("conversation__user-right-name-date" );
+
+
+    const userNameDisplay = document.createElement("div");
+    const userNameText = document.createElement("p");
+    userNameText.innerText = userDetail[i].name;
+    userNameText.classList.add("conversation__user-right-name-display--text");
+    userNameDisplay.appendChild(userNameText);
+    userNameDate.appendChild(userNameDisplay);
+    userCommentNameDate.appendChild(userNameDate);
+    conversationUserRight.appendChild(userCommentNameDate);
+
+    const userDateDisplay = document.createElement("div");
+    const userDateText = document.createElement("p");
+    userDateText.innerText = userDetail[i].date;
+    userDateText.classList.add("conversation__user-right-date-display--text");
+    userDateDisplay.appendChild(userDateText);
+    userNameDate.appendChild(userDateDisplay);
+    userCommentNameDate.appendChild(userNameDate);
+    conversationUserRight.appendChild(userCommentNameDate);
+
+
+    const userCommentDisplay = document.createElement("div");
+    const userCommentText = document.createElement("p");
+    userCommentText.innerText = userDetail[i].commentText;
+    userCommentText.classList.add("conversation__user-right-comment-display--text" );
+    userCommentDisplay.appendChild(userCommentText);
+    userCommentNameDate.appendChild(userCommentDisplay);
+    conversationUserRight.appendChild(userCommentNameDate);
+
+
+
+    conversationUser.appendChild(conversationUserRight);
   }
 }
 
-function createDetail(name,date,comment,image) {
+function createDetail(name, date, comment, image) {
   return {
     name: name,
     date: date,
     commentText: comment,
-    image:image,
+    image: image,
   };
 }
 
@@ -71,9 +98,14 @@ conversationForm.addEventListener("submit", (event) => {
 
   const comment = document.getElementById("conversation__comment").value;
   const name = document.getElementById("conversation__name").value;
-  const date =  currentdate.getMonth() + "/" + currentdate.getDate() +"/" + currentdate.getFullYear();
-  const image =("./assets/images/Mohan-muruge.jpg");
-  userDetail.unshift(createDetail(name,date,comment,image));
+  const date =
+    currentdate.getMonth() +
+    "/" +
+    currentdate.getDate() +
+    "/" +
+    currentdate.getFullYear();
+  const image = "./assets/images/Mohan-muruge.jpg";
+  userDetail.unshift(createDetail(name, date, comment, image));
   userDetail.pop();
 
   conversationDetail();
